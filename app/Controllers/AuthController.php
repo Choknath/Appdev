fa412b42aeb00da43fe264fddeb1b4fe3c603b1f
Cris Paul Mavida <130685008+Choknath@users.noreply.github.com>
2023-11-12 12:01:32 +0800

    get data

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -6,12 +6,21 @@ use App\Controllers\BaseController;
 use CodeIgniter\RestFul\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Models\UserModel;
-
+use App\Models\PostModel;
 
 class AuthController extends ResourceController
 {
-    public function index()
+    public function getData()
     {
-        //
+        // Retrieve data in the databse
+        $user = new UserModel();
+        $user = $user->findAll();
+
+        // Return the user data in the database as JSON
+        return $this->respond($user, 200);
+        
+        // this will check if the database ay may laman
+        // var_dump($user);
     }
 }
+
