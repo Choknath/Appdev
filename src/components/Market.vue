535d740b7f00aa536a4d8a8ed39478c3d10cc727
Cris Paul Mavida <130685008+Choknath@users.noreply.github.com>
2023-11-15 02:27:14 +0800

    market.vue

--- src/components/Market.vue
+++ src/components/Market.vue
@@ -1,66 +1,11 @@
 <template>
-    <v-layout class="overflow-visible" style="height: 56px;">
-    <v-bottom-navigation v-model="value" color="teal" grow>
-  <v-btn>
-    <v-icon>
-      <a href="/Home">
-        <div class="icon" style="font-size: 16px;"> <!-- Adjust the font-size as needed -->
-          <i class="fa fa-users" aria-hidden="true"></i>
-        </div>
-        <div class="text" data-type="Community" style="font-size: 12px;"> <!-- Adjust the font-size as needed -->
-          Community
-        </div>
-      </a>
-    </v-icon>
-  </v-btn>
+    
 
-  <v-btn>
-    <v-icon>
-      <a href="/Event">
-        <div class="icon" style="font-size: 16px;"> <!-- Adjust the font-size as needed -->
-          <i class="fa fa-calendar" aria-hidden="true"></i>
-        </div>
-        <div class="text" data-type="Event" style="font-size: 12px;"> <!-- Adjust the font-size as needed -->
-          Event
-        </div>
-      </a>
-    </v-icon>
-  </v-btn>
-
-  <v-btn>
-    <v-icon>
-      <a href="/Market">
-        <div class="icon" style="font-size: 16px;"> <!-- Adjust the font-size as needed -->
-          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-        </div>
-        <div class="text" data-type="Market" style="font-size: 12px;"> <!-- Adjust the font-size as needed -->
-          Market
-        </div>
-      </a>
-    </v-icon>
-  </v-btn>
-
-  <v-btn>
-    <v-icon>
-      <a href="Profile">
-        <div class="icon" style="font-size: 16px;"> <!-- Adjust the font-size as needed -->
-          <i class="fa fa-user"></i>
-        </div>
-        <div class="text" data-type="Profile" style="font-size: 12px;"> <!-- Adjust the font-size as needed -->
-          Profile
-        </div>
-      </a>
-    </v-icon>
-  </v-btn>
-</v-bottom-navigation>
-  </v-layout>
-  
   <div>
     <section class="market">
       <h2>E-commerce Section</h2>
       <div class="product-catalog">
         <div class="product" v-for="(product, index) in products" :key="index">
-
           <img :src="product.image" :alt="product.name" />
           <h3>{{ product.name }}</h3>
           <p>{{ product.description }}</p>
@@ -68,16 +13,71 @@
           <button @click="addToCart(product)">Add to Cart</button>
       </div>
       </div>
-    </section>
+      </section>
   </div>
+
+  <v-layout class="overflow-visible" style="height: 56px;">
+      <v-bottom-navigation v-model="value" color="teal" grow>
+    <v-btn>
+      <v-icon>
+        <a href="/Home">
+          <div class="icon" style="font-size: 16px;"> <!-- Adjust the font-size as needed -->
+            <i class="fa fa-users" aria-hidden="true"></i>
+          </div>
+          <div class="text" data-type="Community" style="font-size: 12px;"> <!-- Adjust the font-size as needed -->
+            Community
+          </div>
+        </a>
+      </v-icon>
+    </v-btn>
+  
+    <v-btn>
+      <v-icon>
+        <a href="/Event">
+          <div class="icon" style="font-size: 16px;"> <!-- Adjust the font-size as needed -->
+            <i class="fa fa-calendar" aria-hidden="true"></i>
+          </div>
+          <div class="text" data-type="Event" style="font-size: 12px;"> <!-- Adjust the font-size as needed -->
+            Event
+          </div>
+        </a>
+      </v-icon>
+    </v-btn>
+  
+    <v-btn>
+      <v-icon>
+        <a href="/Market">
+          <div class="icon" style="font-size: 16px;"> <!-- Adjust the font-size as needed -->
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+          </div>
+          <div class="text" data-type="Market" style="font-size: 12px;"> <!-- Adjust the font-size as needed -->
+            Market
+          </div>
+        </a>
+      </v-icon>
+    </v-btn>
+  
+    <v-btn>
+      <v-icon>
+        <a href="Profile">
+          <div class="icon" style="font-size: 16px;"> <!-- Adjust the font-size as needed -->
+            <i class="fa fa-user"></i>
+          </div>
+          <div class="text" data-type="Profile" style="font-size: 12px;"> <!-- Adjust the font-size as needed -->
+            Profile
+          </div>
+        </a>
+      </v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+    </v-layout>
+    
 </template>
 
 <script>
 export default {
   data() {
- HEAD
     return {
-      value: 1,
       products: [
         {
           name: "Product 1",
