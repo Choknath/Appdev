f5d7fd12cad0805ca4f6199b3d0e0efce0a471a8
lesterancheta <130680522+lesterancheta@users.noreply.github.com>
2023-11-15 15:27:52 +0800

    ....

---
+++ dist/js/nav.js
@@ -0,0 +1,4 @@
+// Toggle the mobile menu when the hamburger icon is clicked
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    document.getElementById('menu').classList.toggle('active');
+});
