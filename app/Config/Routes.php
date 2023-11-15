4959ea1fee1245782d5c9173daf5e013ce56c647
lesterancheta <130680522+lesterancheta@users.noreply.github.com>
2023-11-15 13:14:27 +0800

    ..

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->match(['post','get']'/login', 'UserController::login');
 $routes->get('/getData', 'AuthController::getData');
