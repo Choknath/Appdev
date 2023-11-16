7d7890f76152c4dfb12e2654961152cb94ea8980
Cris Paul Mavida <130685008+Choknath@users.noreply.github.com>
2023-11-16 21:29:24 +0800

    register

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,104 +1,118 @@
-<template> 
+<template>
+  <div class="container d-flex align-items-center justify-content-center h-100">
+    <div class="v-container theme-container">
+    <v-sheet  width="300" class="mx-auto" >
+  <v-form fast-fail @submit.prevent="register" class="text-center">
+      <div v-if="message === 'error'">Invalid response</div>
 
-  <v-sheet width="300" class="mx-auto"> 
+      <v-text-field v-model="username" label="Username"></v-text-field>
+      <v-text-field v-model="email" label="Email"></v-text-field>
+      <v-text-field v-model="fullname" label="Full Name"></v-text-field>
+      <v-text-field v-model="password" label="Password" type="password"></v-text-field>
+      <v-text-field v-model="passwordConfirm" label="Password Confirm" type="password"></v-text-field>
 
-  <v-form fast-fail @submit.prevent="register"> 
+      <div v-if="message === 'passwordMismatch'">Passwords do not match</div>
 
-      <div v-if="message === 'error'">Invalid response</div> 
-
-      
-
-      <v-text-field v-model="username" label="Username"></v-text-field> 
-
-      <v-text-field v-model="password" label="Password" type="password"></v-text-field> 
-
-      <v-text-field v-model="passwordConfirm" label="Password Confirm" type="password"></v-text-field> 
-
-
-
-      <div v-if="message === 'passwordMismatch'">Passwords do not match</div> 
-
+      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+      <router-link to="/">Login</router-link>
+    </v-form>
+  </v-sheet>
+    </div>
+  </div>
+  </template>
   
-
-      <v-btn type="submit" block class="mt-2">Submit</v-btn> 
-
-      <router-link to="/login">Login</router-link> 
-
-  </v-form> 
-
-  </v-sheet> 
-
-</template> 
-
-
-
-<script> 
-
-import router from '@/router'; 
-import axios from 'axios';
-
-
-
-
-
-export default { 
-
-  data() { 
-
-  return { 
-
-      username: '', 
-
-      password: '', 
-
-      passwordConfirm: '', 
-
-      message: [], 
-
-  }; 
-
-  }, 
-
-  methods: { 
-
-  async register() { 
-
-      if (this.password === this.passwordConfirm) { 
-
-      const data = await axios.post("api/register", { 
-
-          username: this.username, 
-
-          password: this.password 
-
-      }); 
-
-
-
-      this.message = data.data.msg; 
-
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        username: '',
+        email: '',
+        fullname: '',
+        password: '',
+        passwordConfirm: '',
+      message: null,
       
-
-      if (data.data.msg === 'okay') { 
-
-          // sessionStorage.setItem("jwt", data.data.token) 
-
-          alert("Registered successfully"); 
-
-          router.push('/login'); 
-
-      } 
-
-      } else { 
-
-      this.message = "passwordMismatch"; 
-
-      } 
-
+      };
+    },
+    methods: {
+      async register() {
+        try {
+  const response = await axios.post('register', {
+    username: this.username,
+    email: this.email,
+    fullname: this.fullname,
+    password: this.password,
+  });
+  // Assuming your backend sends a JSON response with a 'success' property
+  if (response.data.success) {
+    // Registration successful
+    console.log('Registration successful');
+    // You can redirect or show a success message here
   } 
+  else {
+    // Registration failed
+    console.error('Registration failed');
+    // You might want to set an error message or handle it in some way
+  }
+}
+  catch (error) {
+  // Handle other errors, such as network issues or server errors
+  console.error('An error occurred during registration:', error);
+  // You might want to set an error message or handle it in some way
+}
 
-  } 
+        //     try {
+    //     // Check if passwords match
+    //     if (this.password !== this.passwordConfirm) {
+    //       this.message = 'passwordMismatch';
+    //       return;
+    //     }
 
-}; 
+    //     // Make the API call to your CodeIgniter 4 backend
+    //     const response = await axios.post('register', {
+    //       username: this.username,
+    //       email: this.email,
+    //       fullname: this.fullname,
+    //       password: this.password,
+    //     });
 
-</script>
+    //     // Handle the response from the backend
+    //     if (response.data.success) {
+    //       // Registration successful
+    //       console.log('Registration successful');
+    //       // You can redirect or show a success message here
+    //     } else {
+    //       // Registration failed
+    //       console.error('Registration failed');
+    //       this.message = 'error';
+    //     }
+    //   } catch (error) {
+    //     // Handle other errors, such as network issues or server errors
+    //     console.error('An error occurred during registration:', error);
+    //     this.message = 'error';
+    //   }
+      
+        // if (this.password === this.passwordConfirm) {
+        //   const data = await axios.post('register', {
+        //     username: this.username,
+        //     email: this.email,
+        //     fullname: this.fullname,
+        //     password: this.password,
+        //   });
+  
+        //   this.message = data.data.msg;
+  
+        //   if (data.data.msg === 'okay') {
+        //     // sessionStorage.setItem("jwt", data.data.token)
+        //     alert('Registered successfully');
+        //     router.push('/');
+        //   }
+        // } else {
+        //   this.message = 'passwordMismatch';
+        // }
+      },
+    },
+  };
+  </script>
--- src/views/RegistrationPage.vue
+++ src/views/RegistrationPage.vue
@@ -1,59 +1,18 @@
 <template>
-    <v-sheet width="300" class="mx-auto">
-      <v-form fast-fail @submit.prevent="register">
-        <div v-if="message === 'error'">Invalid response</div>
-  
-        <v-text-field v-model="username" label="Username"></v-text-field>
-        <v-text-field v-model="email" label="Email"></v-text-field>
-        <v-text-field v-model="fullname" label="Full Name"></v-text-field>
-        <v-text-field v-model="password" label="Password" type="password"></v-text-field>
-        <v-text-field v-model="passwordConfirm" label="Password Confirm" type="password"></v-text-field>
-  
-        <div v-if="message === 'passwordMismatch'">Passwords do not match</div>
-  
-        <v-btn type="submit" block class="mt-2">Submit</v-btn>
-        <router-link to="/">Login</router-link>
-      </v-form>
-    </v-sheet>
-  </template>
-  
-  <script>
-  import router from '@/router';
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        username: '',
-        email: '',
-        fullname: '',
-        password: '',
-        passwordConfirm: ''
-      
-      };
-    },
-    methods: {
-      async register() {
-        if (this.password === this.passwordConfirm) {
-          const data = await axios.post('register', {
-            username: this.username,
-            email: this.email,
-            fullname: this.fullname,
-            password: this.password,
-          });
-  
-          this.message = data.data.msg;
-  
-          if (data.data.msg === 'okay') {
-            // sessionStorage.setItem("jwt", data.data.token)
-            alert('Registered successfully');
-            router.push('/');
-          }
-        } else {
-          this.message = 'passwordMismatch';
-        }
-      },
-    },
-  };
-  </script>
-  
+  <Registration />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import Registration from '../components/Registration.vue';
+
+export default defineComponent({
+  name: 'RegistrationPage',
+  components: {
+    Registration,
+  },
+});
+</script>
+
