9b674e962f4dcd458092e466f3942cf196c0ecca
Cris Paul Mavida <130685008+Choknath@users.noreply.github.com>
2023-11-18 15:51:10 +0800

    updated signin and register

--- src/components/signin.vue
+++ src/components/signin.vue
@@ -8,43 +8,35 @@
           <v-text-field v-model="username" label="Username"></v-text-field>
           <v-text-field v-model="password" label="Password" type="password"></v-text-field>
 
-          <v-btn type="submit" block class="mt-3">Submit</v-btn>
+          <v-btn type="submit" block class="mt-3">Login</v-btn>
           <router-link to="/Registration" class="d-block mt-2">Register</router-link>
         </v-form>
       </v-sheet>
     </div>
   </div>
-</template>
+</template> 
 
 <script>
 import axios from 'axios';
+import router from '@/router';
 
 export default {
   data() {
     return {
       username: '',
       password: '',
-      message: '',
+      erroeMsg: '',
     };
   },
   methods: {
     async login() {
-      try {
-        const { data } = await axios.post('/login', {
-          username: this.username,
-          password: this.password,
-        });
-        
-        this.message = data.msg;
-
-        if (data.msg === 'okay') {
-          sessionStorage.setItem('jwt', data.token);
-          router.push('/');
-        }
-      } catch (error) {
-        console.error('An error occurred during login:', error);
-        this.message = 'error';
-      }
+     const d = await axios.post('/login', {
+     username: this.username,
+     password: this.password
+     });
+     if(d.data.msg ==='okay'){
+      router.push('/Home')
+     }
     },
   },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignPage from '../views/SignPage.vue'
 
-
 const routes = [
   
   {
@@ -42,12 +41,7 @@ const routes = [
   // this generates a separate chunk (about.[hash].js) for this route
   // which is lazy-loaded when the route is visited.
   component: () => import(/* webpackChunkName: "about" */ '../views/ProfilePage.vue')
-  },
-  {
-    path: '/EditProf',
-    component: () => import(/* webpackChunkName: "about" */ '../views/EditProfile.vue')
-  },
-  
+  }
 ]
 
 const router = createRouter({
