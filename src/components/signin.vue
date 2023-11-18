b51aaa273cd8ee468d7c06a4b64cae69466afccd
mjlaudencia <laudenciamj3@gmail.com>
2023-11-18 15:54:18 +0800

    Update signin.vue

--- src/components/signin.vue
+++ src/components/signin.vue
@@ -1,51 +1,42 @@
 <template>
-  <div class="container d-flex align-items-center justify-content-center h-100">
-    <div class="v-container theme-container">
-      <v-sheet width="300" class="mx-auto">
-        <v-form @submit.prevent="login">
-          <div v-if="message === 'error'" class="error-message">Invalid response</div>
-
-          <v-text-field v-model="username" label="Username"></v-text-field>
-          <v-text-field v-model="password" label="Password" type="password"></v-text-field>
-
-          <v-btn type="submit" block class="mt-3">Submit</v-btn>
-          <router-link to="/Registration" class="d-block mt-2">Register</router-link>
-        </v-form>
-      </v-sheet>
-    </div>
+  <div class="login-container">
+    <v-container class="theme-container">
+      <v-row>
+        <v-col cols="12" sm="6" md="4" lg="3">
+          <v-sheet>
+            <!-- Your existing form content goes here -->
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+// Import statements go here
 
 export default {
-  data() {
-    return {
-      username: '',
-      password: '',
-      message: '',
-    };
-  },
-  methods: {
-    async login() {
-      try {
-        const { data } = await axios.post('/login', {
-          username: this.username,
-          password: this.password,
-        });
-        
-        this.message = data.msg;
-
-        if (data.msg === 'okay') {
-          sessionStorage.setItem('jwt', data.token);
-          router.push('/');
-        }
-      } catch (error) {
-        console.error('An error occurred during login:', error);
-        this.message = 'error';
-      }
-    },
-  },
+  // Component options go here
 };
 </script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: url('/path/to/your/background-image.jpg');
+  background-size: cover; /* Adjust as needed */
+  background-position: center; /* Adjust as needed */
+}
+
+.theme-container {
+  background-color: rgba(255, 255, 255, 0.8); /* Set a background color with opacity if needed */
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+/* Add your existing styles here */
+</style>
